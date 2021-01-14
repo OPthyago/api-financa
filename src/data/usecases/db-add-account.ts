a6d1c25e4e78d28cb97d38db2272ed285f23fab3
Thyago de Oliveira Pereira <thyagoop@gmail.com>
2021-01-14 12:32:58 -0300

    feat: ensure DbAddAccount calls Encrypter with correct value

---
+++ src/data/usecases/db-add-account.ts
@@ -0,0 +1,19 @@
+import { AccountModel } from '../../domain/models/account'
+import { AddAccount, AddAccountModel } from '../../domain/usecases/add-account'
+import { Encrypter } from '../protocols/encryper'
+
+export class DbAddAccount implements AddAccount {
+  private readonly encrypter: Encrypter
+
+  constructor (encrypter: Encrypter) {
+    this.encrypter = encrypter
+  }
+
+  async add (account: AddAccountModel): Promise<AccountModel> {
+    await this.encrypter.encrypt(account.password)
+    return {
+      id: '',
+      ...account
+    }
+  }
+}
