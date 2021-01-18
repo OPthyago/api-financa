28cf9624e2ddc1573cf86003679d2ca434c0db96
Thyago de Oliveira Pereira <thyagoop@gmail.com>
2021-01-18 12:44:44 -0300

    feat: add LogControllerDecorator to SignUp factory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,12 +3,17 @@ import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { SignUpController } from '../../presentation/controller/signup/signup'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { AccountPrismaRepository } from '../../infra/db/psql/repository/account-repository/account'
+import { LogControllerDecorator } from '../decorators/log'
+import { LogPrismaRepository } from '../../infra/db/psql/repository/log-repository/log-repository'
+import { Controller } from '../../presentation/protocols'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountPrismaRepository = new AccountPrismaRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountPrismaRepository)
-  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const logRepository = new LogPrismaRepository()
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  return new LogControllerDecorator(signUpController, logRepository)
 }
